0fe8b66964371a469f056b09cc808ef97f134334
DESKTOP-LA3KBVD\It <shuaib6950@gmail.com>
2026-01-05 15:28:42 +0600

    fix create collection dto

--- src/main/java/com/personal/lend_rack/model/dto/CreateBookCollectionRequest.java
+++ src/main/java/com/personal/lend_rack/model/dto/CreateBookCollectionRequest.java
@@ -1,15 +1,5 @@
 package com.personal.lend_rack.model.dto;
 
-public record CreateBookCollectionRequest(String title, String author, String publisher, Long category_id, Long language_id, Long collection_id, String description, int page_count) {
-
-    public String location() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'location'");
-    }
-
-    public String collection_name() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'collection_name'");
-    }
+public record CreateBookCollectionRequest(String collection_name, String location, Long created_by) {
 
 }
